f8304b9872c231e9cb28c431941763b3e02b4c72
Davion Greene <112106262+davion316@users.noreply.github.com>
2022-10-05 21:11:31 -0400

    Update and rename Assignment2.R to assignment2.R

---
+++ assignment2.R
@@ -0,0 +1,8 @@
+Author: "Davion Greene", Date: September 22, 2022, Purpose: To Calculate Correlation Analysis
+my_data <- mtcars 
+head(my_data, 6)
+ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+res <- cor.test(my_data$wt, my_data$mpg, method = "pearson") 
+res
+res2 <-cor.test(my_data$wt, my_data$mpg, method = "spearman")
+res2
